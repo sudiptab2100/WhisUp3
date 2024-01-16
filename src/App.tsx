8fc42ef0ce2b51257ece72a0f3a898c7a6d77203
Sudipta Basak <sudiptab2100@gmail.com>
2024-01-16 14:52:25 +0530

    Able to fetch params from url

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const queryParameters = new URLSearchParams(window.location.search)
+  const user = queryParameters.get("user")
+  console.log(user)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
