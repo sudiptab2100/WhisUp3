e3f90b133ff33607a835467e0a26d862cca13fb9
Sudipta Basak <sudiptab2100@gmail.com>
2024-01-18 01:00:22 +0530

    Stored message button

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { initWaku, sendMessage } from "./components/Waku";
+import {
+  initWaku,
+  sendMessage,
+  getStoredMessage,
+  initMetamask,
+  MessagePair,
+} from "./components/Waku";
 import { LightNode } from "@waku/sdk";
 import SenderForm from "./components/SenderForm";
 
 function App() {
   const queryParameters = new URLSearchParams(window.location.search);
-  const to = (queryParameters.get("to") || '').toString();
+  const to = (queryParameters.get("to") || "").toString();
   console.log(to);
   const [waku, setWaku] = useState<LightNode>();
   const [status, setStatus] = useState<string>("Connecting...");
@@ -15,7 +21,7 @@ function App() {
     const init = async () => {
       const node = await initWaku();
       setWaku(node);
-      if(waku?.isConnected()) setStatus("Connected");
+      if (waku?.isConnected()) setStatus("Connected");
     };
     init();
   });
@@ -26,9 +32,17 @@ function App() {
         <h1>Waku Chat</h1>
         <h2>{status}</h2>
         <SenderForm to={to} waku={waku} onClickSend={sendMessage} />
+        <button onClick={() => getStoredMessages(waku!)}>Get Messages</button>
       </header>
     </div>
   );
 }
 
 export default App;
+
+const getStoredMessages = async (waku: LightNode) => {
+  const user = await initMetamask();
+  console.log(user);
+  const messagePairs = await getStoredMessage(waku, user!);
+  console.log(messagePairs);
+};
